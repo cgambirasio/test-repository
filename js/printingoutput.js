942e2b7b33ea461f7955c0abd9496a623a840b3a
cgambirasio <gamb0076@algonquinlive.com>
2025-01-19 18:35:43 -0500

    Create printingoutput.js

---
+++ js/printingoutput.js
@@ -0,0 +1,31 @@
+// Assignment: Printing Output
+
+// TASK 1
+
+let a = 3;
+let b = 5;
+let c;
+
+let output = `
+// alert( "description of alerted_value: " + alerted_value );
+/*
+${a} + ${b} = ${a + b}
+${a} - ${b} = ${a - b}
+${a} * ${b} = ${a * b}
+${a} / ${b} = ${a / b}
+${a} % ${b} = ${a % b}
+${a} += ${b} = ${a += b}
+${a} -= ${b} = ${a -= b}
+${a} *= ${b} = ${a *= b}
+${a} /= ${b} = ${a /= b}
+${a} %= ${b} = ${a %= b}
+${a} == ${b} = ${a == b}
+${a} != ${b} = ${a != b}
+${a} > ${b} = ${a > b}
+${a} < ${b} = ${a < b}
+!${a} && !${c} = ${!a && !c}
+!${a} || !${c} = ${!a || !c}
+*/
+`;
+
+console.log(output);
